fb6ed136948d7b84ece89a22f5905f97d8d5f8b9
VerMishelb <57833258+VerMishelb@users.noreply.github.com>
2022-03-30 11:15:48 +0300

    Fixed (?) Circle::operator= overload

--- HW3/Circle.cpp
+++ HW3/Circle.cpp
@@ -1,9 +1,9 @@
 #include "Circle.h"
 
-Circle::~Circle() {}
 Circle::Circle() {}
-Circle::Circle(double _r) : r(_r) {}
+Circle::Circle(const double& _r) : r(_r) {}
 Circle::Circle(const Circle& other) : r(other.r) {}
+Circle::~Circle() {}
 
 const Circle Circle::operator+(const Circle& other) const {
     return Circle(r + other.r);
@@ -11,7 +11,7 @@ const Circle Circle::operator+(const Circle& other) const {
 const Circle Circle::operator-(const Circle& other) const {
     return Circle(r - other.r);
 }
-const Circle& Circle::operator=(const double& other) const {
+const Circle& Circle::operator=(const double& other) {
     return Circle(other);
 }
 const bool Circle::operator>(const Circle& other) const {
@@ -19,6 +19,9 @@ const bool Circle::operator>(const Circle& other) const {
 }
 const bool Circle::operator<(const Circle& other) const {
     return (r < other.r);
+}
+Circle::operator double() const {
+    return r;
 }
 std::ostream& operator<<(std::ostream& out, const Circle& val) {
     out << val.r;
--- HW3/Circle.h
+++ HW3/Circle.h
@@ -4,27 +4,30 @@
 #include <iostream>
 
 class Circle {
+private:
     double r = 0;
 
 public:
     Circle();
-    ~Circle();
-    Circle(double r);
+    Circle(const double& r);
     Circle(const Circle& other);
+    ~Circle();
     
-    const double Pi = 3.141592653589793;
-
     const Circle operator+(const Circle& other) const;
     const Circle operator-(const Circle& other) const;
-    const Circle& operator=(const double& other) const;
+    const Circle& operator=(const double& other);
+    Circle& operator=(const Circle& other) = default;
     const bool operator>(const Circle& other) const;
     const bool operator<(const Circle& other) const;
+    operator double() const;
     friend std::ostream& operator<<(std::ostream& out, const Circle& val);
 
     double getArea();
     double getPerimeter();
     void setRadius(double v);
     double getRadius();
+
+    const double Pi = 3.141592653589793;
 };
 
 #endif // !CIRCLE_H
